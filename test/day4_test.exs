64ec130d32174d439102d4e2e54ad3bff32fc465
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-11 15:15:24 -0600

    Add tests of the fourth day of the advent of code

---
+++ test/day4_test.exs
@@ -0,0 +1,20 @@
+defmodule Day4Test do
+  use ExUnit.Case
+  doctest AocElixir
+
+  test "secret key is abcdef, the MD5 hash starting with five zeroes" do
+    assert AocElixir.Day4.find_hash("abcdef", "00000") == "609043"
+  end
+
+  test "secret key is pqrstuv, the MD5 hash starting with five zeroes" do
+    assert AocElixir.Day4.find_hash("pqrstuv", "00000") == "1048970"
+  end
+
+  test "secret key is bgvyzdsv, the MD5 hash starting with five zeroes" do
+    assert AocElixir.Day4.solve_puzzle("00000") == "254575"
+  end
+
+  test "secret key is bgvyzdsv, the MD5 hash starting with six zeroes" do
+    assert AocElixir.Day4.solve_puzzle("000000") == "1038736"
+  end
+end
