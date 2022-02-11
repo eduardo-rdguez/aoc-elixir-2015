b737d48282f96551cfd88d42496e593467a96c4b
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-10 23:18:07 -0600

    Add functions to validate the second puzzle of the first day

--- lib/day1.ex
+++ lib/day1.ex
@@ -8,6 +8,11 @@ defmodule AocElixir.Day1 do
     |> calculate_floor()
   end
 
+  def second_puzzle do
+    read_test_file()
+    |> find_position()
+  end
+
   defp read_test_file do
     File.read!("resources/day1.txt")
   end
@@ -15,7 +20,20 @@ defmodule AocElixir.Day1 do
   def calculate_floor(instrucctions) do
     instrucctions
     |> String.split("", trim: true)
-    |> Enum.reduce(0, fn instrucction, floor -> elevator(instrucction) + floor end)
+    |> Enum.reduce(0, fn instrucction, current_floor -> current_floor + elevator(instrucction) end)
+  end
+
+  def find_position(instrucctions) do
+    instrucctions
+    |> String.split("", trim: true)
+    |> calculate_floor(0, 0)
+  end
+
+  defp calculate_floor(_, -1, position), do: position
+
+  defp calculate_floor([instrucction | instrucctions], current_floor, position) do
+    new_floor = elevator(instrucction) + current_floor
+    calculate_floor(instrucctions, new_floor, position + 1)
   end
 
   defp elevator(")"), do: -1
