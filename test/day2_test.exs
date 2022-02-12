22fbf623ce139bfd83133dd99747bdbeb3575c51
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-11 20:28:30 -0600

    Add tests of the second day of the advent of code

---
+++ test/day2_test.exs
@@ -0,0 +1,28 @@
+defmodule Day2Test do
+  use ExUnit.Case
+  doctest AocElixir
+
+  test "A present with dimensions 2x3x4 requires a total of 58 square feet" do
+    assert AocElixir.Day2.calculate_required_wrapping(["2x3x4"]) == 58
+  end
+
+  test "A present with dimensions 1x1x10 requires a total of 42 square feet" do
+    assert AocElixir.Day2.calculate_required_wrapping(["1x1x10"]) == 43
+  end
+
+  test "How many total square feet of wrapping paper should they order?" do
+    assert AocElixir.Day2.first_puzzle() == 1_606_483
+  end
+
+  test "A present with dimensions 2x3x4 requires a total of 34 feet" do
+    assert AocElixir.Day2.calculate_required_ribbon(["2x3x4"]) == 34
+  end
+
+  test "A present with dimensions 1x1x10 requires a total of 14 feet" do
+    assert AocElixir.Day2.calculate_required_ribbon(["1x1x10"]) == 14
+  end
+
+  test "How many total feet of ribbon should they order?" do
+    assert AocElixir.Day2.second_puzzle() == 3_842_356
+  end
+end
