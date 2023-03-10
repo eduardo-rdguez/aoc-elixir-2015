5e3c56c08274a0615461363b079f01002ed2113b
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-03-10 12:43:03 -0600

    Fixes misspelled letters in the day one module

--- lib/day1.ex
+++ lib/day1.ex
@@ -17,23 +17,23 @@ defmodule AocElixir.Day1 do
     File.read!("resources/day1.txt")
   end
 
-  def calculate_floor(instrucctions) do
-    instrucctions
+  def calculate_floor(instructions) do
+    instructions
     |> String.split("", trim: true)
-    |> Enum.reduce(0, fn instrucction, current_floor -> current_floor + elevator(instrucction) end)
+    |> Enum.reduce(0, fn instruction, current_floor -> current_floor + elevator(instruction) end)
   end
 
-  def find_position(instrucctions) do
-    instrucctions
+  def find_position(instructions) do
+    instructions
     |> String.split("", trim: true)
     |> calculate_floor(0, 0)
   end
 
   defp calculate_floor(_, -1, position), do: position
 
-  defp calculate_floor([instrucction | instrucctions], current_floor, position) do
-    new_floor = elevator(instrucction) + current_floor
-    calculate_floor(instrucctions, new_floor, position + 1)
+  defp calculate_floor([instruction | instructions], current_floor, position) do
+    new_floor = elevator(instruction) + current_floor
+    calculate_floor(instructions, new_floor, position + 1)
   end
 
   defp elevator(")"), do: -1
