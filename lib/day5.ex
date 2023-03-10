b96b7e3b4e150b4bfeffae6cb8066afd022eac5f
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-03-10 12:41:24 -0600

    Add solution of the second part of the puzzle of day five

--- lib/day5.ex
+++ lib/day5.ex
@@ -4,22 +4,29 @@ defmodule AocElixir.Day5 do
   """
 
   @vowels ~r/(.*a.*|.*e.*|.*i.*|.*o.*|.*u.*){3}/
-  @disallowed ~r/(ab|cd|pq|xy)/
+  @disallowed_substrings ~r/(ab|cd|pq|xy)/
   @appears_twice ~r/(.)\1+/
+  @two_letter_appears_twice ~r/(\w{2})\w*(\1)(?!\2)/
+  @letter_between_them ~r/(\w)\w\1\w?/
 
   def first_part() do
     read_test_file()
     |> find_nice_strings()
   end
 
+  def second_part() do
+    read_test_file()
+    |> find_nice_strings_v2()
+  end
+
   defp read_test_file() do
     File.read!("resources/day5.txt")
   end
 
-  def find_nice_strings(strings) do
+  defp find_nice_strings(strings) do
     strings
     |> String.split("\n", trim: true)
-    |> Enum.map(fn string -> is_nice_string(string) end)
+    |> Enum.map(&is_nice_string/1)
     |> Enum.count(& &1)
   end
 
@@ -41,6 +48,28 @@ defmodule AocElixir.Day5 do
 
   defp contains_disallowed_substrings(string) do
     string
-    |> String.match?(@disallowed)
+    |> String.match?(@disallowed_substrings)
+  end
+
+  defp find_nice_strings_v2(strings) do
+    strings
+    |> String.split("\n", trim: true)
+    |> Enum.map(&is_nice_string_v2/1)
+    |> Enum.count(& &1)
+  end
+
+  def is_nice_string_v2(string) do
+    contains_a_pair_two_letters_that_appears_twice(string) &&
+      contains_one_letter_that_repeats_with_one_letter_between_them(string)
+  end
+
+  defp contains_a_pair_two_letters_that_appears_twice(string) do
+    string
+    |> String.match?(@two_letter_appears_twice)
+  end
+
+  defp contains_one_letter_that_repeats_with_one_letter_between_them(string) do
+    string
+    |> String.match?(@letter_between_them)
   end
 end
