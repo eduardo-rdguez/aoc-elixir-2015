d4eda331e0aa927b40f85f990886b1bbe1b0ff51
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-03-10 12:56:28 -0600

    Update project dependencies

--- mix.exs
+++ mix.exs
@@ -32,9 +32,9 @@ defmodule AocElixir.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:b58, "~> 1.0.3"},
-      {:flow, "~> 1.2.0"},
-      {:ex_doc, "~> 0.28.2", runtime: false},
-      {:excoveralls, "~> 0.14.4", only: :test}
+      {:flow, "~> 1.2.3"},
+      {:ex_doc, "~> 0.29.2", runtime: false},
+      {:excoveralls, "~> 0.16.0", only: :test}
     ]
   end
 end
